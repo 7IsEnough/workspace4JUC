9516e4bff8075371de2685094ca24dbd310c7168
Promise <976949689@qq.com>
2022-06-27 23:09:16 +0800

    2022年6月27日23:09:12

---
+++ juc/src/main/java/com/clearlove/demo01/Test1.java
@@ -0,0 +1,14 @@
+package com.clearlove.demo01;
+
+/**
+ * @author promise
+ * @date 2022/6/27 - 22:36
+ */
+public class Test1 {
+
+  public static void main(String[] args) {
+    // 获取CPU的核数
+    // CPU密集型，IO密集型
+    System.out.println(Runtime.getRuntime().availableProcessors());
+  }
+}
